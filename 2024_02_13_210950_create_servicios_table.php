f3d96fdd49c61659f60cf911e2141754101ec563
FranciscoInsuasty <141275807+FranciscoInsuasty@users.noreply.github.com>
2024-02-14 10:23:56 -0500

    commit mipagoya

---
+++ 2024_02_13_210950_create_servicios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->integer('idservicio', true);
+            $table->string('Nombre', 45);
+            $table->string('Email', 100);
+            $table->string('Mensaje', 200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
